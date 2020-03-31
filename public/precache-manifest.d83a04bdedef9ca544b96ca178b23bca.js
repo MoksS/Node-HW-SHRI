9c864ee9c136c71a20cfc69f642a335198763545
MoksS <MoksShuv@yandex.ru>
2020-03-31 15:26:11 +0300

    дабавил билд из фронта в public сервера

---
+++ public/precache-manifest.d83a04bdedef9ca544b96ca178b23bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69916621befa88105f215ba23805fd0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652c98b5306bcdb5ab03",
+    "url": "/static/css/main.c665b87f.chunk.css"
+  },
+  {
+    "revision": "acd3b882b6da3fbe2e96",
+    "url": "/static/js/2.55e241b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.55e241b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652c98b5306bcdb5ab03",
+    "url": "/static/js/main.7badc25d.chunk.js"
+  },
+  {
+    "revision": "a9826cacf95ce8ad1ce1",
+    "url": "/static/js/runtime-main.6e4d6195.js"
+  }
+]);
